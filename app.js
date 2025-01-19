84f44ac86ed0cc3597ee0bd05bd9f4130095fb42
Fausto Sacufundala <155498985+fausto18@users.noreply.github.com>
2025-01-19 11:10:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {volume: 1, rate: 1, pitch: 1});
 }
 
 function exibirMensagemInicial() {
